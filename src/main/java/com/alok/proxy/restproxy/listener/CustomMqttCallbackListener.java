61bb76b1048b35d750572e61461c8021661bfec1
alokkusingh <alok.ku.singh@gmail.com>
2022-08-08 21:36:31 +0530

    Updated completable future response

--- src/main/java/com/alok/proxy/restproxy/listener/CustomMqttCallbackListener.java
+++ src/main/java/com/alok/proxy/restproxy/listener/CustomMqttCallbackListener.java
@@ -1,6 +1,7 @@
 package com.alok.proxy.restproxy.listener;
 
 import com.alok.mqtt.listener.MqttCallbackListener;
+import com.alok.mqtt.payload.RequestPayload;
 import com.alok.mqtt.payload.ResponsePayload;
 import com.alok.mqtt.processor.RequestProcessor;
 import com.alok.mqtt.processor.ResponseProcessor;
@@ -8,6 +9,8 @@ import com.alok.mqtt.service.MqttClientService;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.util.Optional;
 
 @Slf4j
 public class CustomMqttCallbackListener extends MqttCallbackListener {
@@ -25,13 +28,17 @@ public class CustomMqttCallbackListener extends MqttCallbackListener {
     @Override
     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
         log.info("Message arrived - {} - {}", topic, mqttMessage);
+        ResponsePayload responsePayload = null;
         try {
-            ResponsePayload responsePayload = this.responseProcessor.parseMqttRequest(mqttMessage.getPayload());
-            log.info("ResponsePayload - {}", responsePayload);
-            responseProcessor.processResponse(responsePayload);
+            responsePayload = this.responseProcessor.parseMqttRequest(mqttMessage.getPayload());
+
         } catch (Exception e) {
             log.error("Error processing payload, error: {}", e.getMessage());
             e.printStackTrace();
+        } finally {
+            responsePayload = Optional.ofNullable(responsePayload).orElse(new ResponsePayload());
+            log.info("ResponsePayload - {}", responsePayload);
+            responseProcessor.processResponse(responsePayload);
         }
     }
 }
